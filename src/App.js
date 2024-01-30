fad34c4d93223ce182723ece3dda147f4b1de386
shrikantgarg17 <56015673+shrikantgarg17@users.noreply.github.com>
2024-01-30 12:13:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import BlogTable from './Components/Blog/BlogTable';
+
+function App() {
+  return (
+   <div>
+    <div><BlogTable />
+    </div>
+
+   </div>
+
+  );
+}
+
+export default App;
